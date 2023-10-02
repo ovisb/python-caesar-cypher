f754a480a7a808445ae1e62b072460a456861524
Ovidiu <ovidiu.sbangu@gmail.com>
2023-10-02 15:59:29 +0300

    Complete stage2 -> creating cypher via shift modifier + added stage2 tests

--- caesar_cypher/caesar_cypher.py
+++ caesar_cypher/caesar_cypher.py
@@ -6,6 +6,16 @@ def get_user_input() -> str:
     return input()
 
 
+def convert_to_cypher(test_input: str, shift_modifier: int = 3) -> str:
+    new = []
+    for char in test_input.split():
+        char_idx = string.ascii_lowercase.index(char)
+        mod_idx = (char_idx + shift_modifier) % 26
+        new.append(string.ascii_lowercase[mod_idx])
+
+    return "".join(new)
+
+
 def convert_to_int(user_input: str) -> list[int]:
     return [int(num) for num in user_input.split()]
 
