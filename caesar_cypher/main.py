67aa02517169fda0b42f650bf282cd07adfec6a3
Ovidiu <ovidiu.sbangu@gmail.com>
2023-10-04 16:51:20 +0300

    Completed Stage4.

--- caesar_cypher/main.py
+++ caesar_cypher/main.py
@@ -1,13 +1,23 @@
 """Main module"""
-from caesar_cypher import decode_text, find_shift, get_user_input  # type: ignore
+from caesar_cypher import decode_caesar, find_shift, get_user_input, get_key_length, get_indices_difference, decode_vigenere  # type: ignore
 
 
 def main() -> None:
+    print("Enter key length: ", end="")
+    key_len = get_key_length()
+    print("Enter plain text: ", end="")
+    regular_text = get_user_input()
+    print("Enter encoded text: ", end="")
     encoded_text = get_user_input()
-    text_to_decode = get_user_input()
-    shift = find_shift(encoded_text)
-    print(shift)
-    print(decode_text(text_to_decode, shift))
+    print("Enter target message to decode: ", end="")
+    target_message = get_user_input()
+
+    new_indices = get_indices_difference(regular_text, encoded_text)
+
+    # Slice indices up to max key_len
+    new_indices_up_to_key = new_indices[:key_len]
+
+    print(decode_vigenere(target_message, new_indices_up_to_key))
 
 
 if __name__ == "__main__":
